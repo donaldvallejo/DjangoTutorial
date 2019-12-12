217a32fc617ea78a8b1b4210f8f53d717428af61
Donald Vallejo <donny.vallejo@gmail.com>
2019-12-12 01:31:03 -0800

    getting strted on page 5 for tests

---
+++ mysite/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/localscrub/dev/makeSchool/BEW/1.2/DjangoTutorial/mysite/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
